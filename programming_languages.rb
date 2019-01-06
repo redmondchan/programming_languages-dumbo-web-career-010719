232cc8d858eac36a9d5cc93f61a5b4616f31cdc7
Redmond Chan <redmondchan92@gmail.com>
2019-01-06 20:12:38 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,12 +1,12 @@
 def reformat_languages(languages)
   new_hash = {}
-  languages.each do |style,data|
+  languages.each do |style, data|
     data.each do |lang_name, describe|
     if new_hash[lang_name] == nil
-      new_hash[lang_name] == describe
-      new_hash[lang_name][:style]=[]
+      new_hash[lang_name] = describe
+      new_hash[lang_name][:style] = []
     end
-    new_hash[lang_name][:style]<<style
+    new_hash[lang_name][:style] << style
   end
 end
 new_hash
